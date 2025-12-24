1e058069d28d52180d46c8130e07165d6597acf4
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-24 11:15:46 +0700

    Membuat AppNavigation dan mengganti Greeting dengan AppNavigation

--- app/src/main/java/com/example/hobbyyk_new/MainActivity.kt
+++ app/src/main/java/com/example/hobbyyk_new/MainActivity.kt
@@ -6,12 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.hobbyyk_new.data.api.RetrofitClient
+import com.example.hobbyyk_new.data.datastore.UserStore
 import com.example.hobbyyk_new.ui.theme.HobbyYKNEWTheme
+import com.example.hobbyyk_new.view.AppNavigation
+import com.example.hobbyyk_new.view.screen.LoginScreen
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +28,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HobbyYKNEWTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    AppNavigation()
                 }
             }
         }
