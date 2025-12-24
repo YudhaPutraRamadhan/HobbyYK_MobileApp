7acabb98ea7f3a814913007d993e7e79c8b7939b
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-24 11:16:01 +0700

    Membuat RetrofitClient dan menambahkan interceptor untuk logging dan otentikasi token

---
+++ app/src/main/java/com/example/hobbyyk_new/data/api/RetrofitClient.kt
@@ -0,0 +1,40 @@
+package com.example.hobbyyk_new.data.api
+
+import com.example.hobbyyk_new.utils.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    var authToken: String? = null
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .addInterceptor { chain ->
+            val original = chain.request()
+            val requestBuilder = original.newBuilder()
+
+            authToken?.let { token ->
+                requestBuilder.header("Authorization", "Bearer $token")
+            }
+
+            val request = requestBuilder.build()
+            chain.proceed(request)
+        }
+        .build()
+
+    val instance: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        retrofit.create(ApiService::class.java)
+    }
+}
