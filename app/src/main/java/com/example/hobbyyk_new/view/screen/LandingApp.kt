a54eb7e5112f4092ff514b3edc7d8ec2b6593adc
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-24 17:05:44 +0700

    Mengubah landing screen dan menambahkan logo untuk Landing Screen

--- app/src/main/java/com/example/hobbyyk_new/view/screen/LandingApp.kt
+++ app/src/main/java/com/example/hobbyyk_new/view/screen/LandingApp.kt
@@ -1,18 +1,31 @@
 package com.example.hobbyyk_new.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement // Import ini
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column // Import ini
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color // Import ini buat custom warna
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.hobbyyk_new.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun LandingApp(
@@ -21,6 +34,8 @@ fun LandingApp(
     val viewModel: LandingAppViewModel = viewModel()
 
     LaunchedEffect(viewModel.startDestination) {
+        delay(2000)
+
         viewModel.startDestination?.let { destination ->
             onNavigate(destination)
         }
@@ -29,14 +44,36 @@ fun LandingApp(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primary),
+            .background(Color(0xFFF5ECDA)),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "HobbyYK",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo Aplikasi",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "HobbyYK",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+
+            Spacer(modifier = Modifier.height(28.dp))
+
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(32.dp)
+            )
+        }
     }
 }
