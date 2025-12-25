13c859d732e2d2917efcf577155c69704c95c72c
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-25 17:03:38 +0700

    Menghapus impor yang tidak digunakan di MainActivity

--- app/src/main/java/com/example/hobbyyk_new/MainActivity.kt
+++ app/src/main/java/com/example/hobbyyk_new/MainActivity.kt
@@ -5,22 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import com.example.hobbyyk_new.data.api.RetrofitClient
-import com.example.hobbyyk_new.data.datastore.UserStore
 import com.example.hobbyyk_new.ui.theme.HobbyYKNEWTheme
 import com.example.hobbyyk_new.view.AppNavigation
-import com.example.hobbyyk_new.view.screen.LoginScreen
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
