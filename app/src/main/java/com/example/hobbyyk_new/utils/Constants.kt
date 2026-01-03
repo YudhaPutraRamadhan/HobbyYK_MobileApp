dc646764035013bef00518a8f5f30f4440d69649
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2026-01-03 20:53:08 +0700

    Mengubah BASE_URL dan URL_GAMBAR_BASE ke `10.0.2.2`

--- app/src/main/java/com/example/hobbyyk_new/utils/Constants.kt
+++ app/src/main/java/com/example/hobbyyk_new/utils/Constants.kt
@@ -1,6 +1,10 @@
 package com.example.hobbyyk_new.utils
 
 object Constants {
+    //const val BASE_URL = "http://10.56.50.45:5000/"
+
+    //const val URL_GAMBAR_BASE = "http://10.56.50.45:5000/uploads/"
+
     const val BASE_URL = "http://10.0.2.2:5000/"
 
     const val URL_GAMBAR_BASE = "http://10.0.2.2:5000/uploads/"
